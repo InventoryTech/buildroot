de7420292b14b42739c44122a3cfeecf8984e8ed
Ashlin Inwood <ashlin@cleverfirstaid.com>
2024-02-22 21:04:38 +0000

    openssh disable binary hardening

--- package/openssh/openssh.mk
+++ package/openssh/openssh.mk
@@ -35,9 +35,9 @@ define OPENSSH_PERMISSIONS
 	/var/empty d 755 root root - - - - -
 endef
 
-ifeq ($(BR2_TOOLCHAIN_HAS_GCC_BUG_110934),y)
+#ifeq ($(BR2_TOOLCHAIN_HAS_GCC_BUG_110934),y)
 OPENSSH_CONF_OPTS += --without-hardening
-endif
+#endif
 
 ifeq ($(BR2_TOOLCHAIN_SUPPORTS_PIE),)
 OPENSSH_CONF_OPTS += --without-pie
